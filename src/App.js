3229573d08292402592be1f41fb98ff48262ec85
devDohyun <dohyun10101@gmail.com>
2021-01-17 14:02:35 +0900

    Init

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>My React Practice</div>
+        )
+    }
+}
+
+export default App
