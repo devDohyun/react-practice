6b4da0b389eb2bec086e0674ac509006f4211575
devDohyun <dohyun10101@gmail.com>
2021-04-13 00:42:51 +0900

    Updated Nav

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -3,11 +3,17 @@ import './index.scss'
 import { Link } from 'react-router-dom'
 
 function Nav() {
-    return (<nav>
-        <div>My React Practice</div>
-        <Link to="/">들어가며</Link>
-        <Link to="/todo-list">Todo List</Link>
-    </nav>)
+    return (<div id="nav">
+        <div className="nav-content">
+            <Link to="/" className="logo">
+                My React Practice
+            </Link>
+            <nav>
+                <Link to="/">들어가며</Link>
+                <Link to="/todo-list">Todo List</Link>
+            </nav>
+        </div>
+    </div>)
 }
 
 export default Nav
